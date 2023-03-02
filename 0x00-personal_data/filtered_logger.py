89d455b8cf34496f1816d0d009fe09820f646b15
mmoh-i <ibrahimmusty100@yahoo.com>
2023-03-02 18:34:44 +0000

    regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,23 +2,13 @@
 import re
 
 
-def filter_datum(
-        fields: str, redaction: str,
+def filter_datum(fields: str, redaction: str,
         message: str, separator: str) -> str:
     """
-    Obfuscates the specified fields in
-    a log message with a redaction string.
-
-    Args:
-    fields: A list of strings representing all fields to obfuscate.
-    redaction: A string representing by what the field will be obfuscated.
-    message: A string representing the log line.
-    separator: A string representing by which character is
-    separating all fields in the log line (message).
-
-    Returns:
-    The obfuscated log message.
+    Obfuscates the specified fields in a log message with
+    a redaction string
     """
-    return re.sub(f'(?<={separator})[^{separator}]*{fields[0]}[^{separator}]\
-            *(?={separator})|(?<={separator})[^{separator}]*{fields[1]}\
-            [^{separator}]*(?={separator})', redaction, message)
+    for field in fields:
+        messge = re.sub(field+'=.*?'+separator,
+            field+'='+redaction+separator, message)
+    return messge
