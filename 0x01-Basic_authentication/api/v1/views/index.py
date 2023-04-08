66e03b179563fef2693c7321a997342c258fe3a4
mmoh-i <ibrahimmusty100@yahoo.com>
2023-04-08 02:24:29 -0700

    ErrorHandler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,9 +5,6 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-app_views = Blueprint('index', __name__)
-
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -29,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'])
+@app_views.route('/unauthorized')
 def unauthorized():
     """ GET /api/v1/unauthorized
     Return:
@@ -38,7 +35,7 @@ def unauthorized():
     abort(401)
 
 
-@app_views.route('/forbidden', methods=['GET'])
+@app_views.route('/forbidden')
 def forbidden():
     """ GET /api/v1/forbidden
     Return:
