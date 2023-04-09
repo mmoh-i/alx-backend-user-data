254ee71a2bfe1bb58e52eceae838936de338eef6
mmoh-i <ibrahimmusty100@yahoo.com>
2023-04-09 11:49:04 -0700

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-import request from flask
-from typing import List
+from flask import request
+from typing import List, TypeVar
 
 
 class Auth:
     def __init__(self):
         pass
     
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+   
+   def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         return False
 
-    def authorization_header(self, request=None) -> str:
+   
+   def authorization_header(self, request=None) -> str:
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
+   
+   def current_user(self, request=None) -> TypeVar('User'):
         return None
