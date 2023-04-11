ee4154342f12a6ea0f14ae1aafb1e51f8c1925ef
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-11 14:46:36 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,8 +58,10 @@ class BasicAuth(Auth):
         email = user_info[0]
         password = user_info[2]
         return email, password
- 
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
         """returns the User instance based on
         email and password"""
         if user_email is None or not isinstance(user_email, str):
