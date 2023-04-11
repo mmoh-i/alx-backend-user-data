3b6db006e83674eadea44846d6cf41071b2dbce3
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-11 11:13:08 +0100

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,17 @@ to inherit from auth
 """
 from flask import request
 from typing import List, TypeVar
+import base64
 
 
 class BasicAuth(Auth):
-  pass
+  def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    """method to return Base64 athorization header.
+    """
+    if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        
+        if not authorization_header.startswith('Basic '):
+            return None
+        
+        return authorization_header.split(' ')[1]
