82bc01fbc402cb0044558d8f2055eb6035a45581
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-11 14:08:09 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,3 +40,18 @@ class BasicAuth(Auth):
             ).decode('utf-8')
         except Exception:
             return None
+
+def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    """eturns the user email and password
+    from the Base64 decoded value.
+    """
+    if decoded_base64_authorization_header is None 
+    and not isinstance(decoded_base64_authorization_header, str):
+        return None, None
+    if ":" not in decoded_base64_authorization_header:
+        return None, None
+    user_info = decoded_base64_authorization_header.partition(":")
+    email = user_info[0]
+    password = user_info[2]
+
+    return email, password
