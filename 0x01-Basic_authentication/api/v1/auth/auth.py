41e7d6d723cbff443d6ae0601d18151f26357794
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-11 09:27:28 +0100

    3. Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -30,13 +30,13 @@ class Auth:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
-        for excluded_path in exluded_paths:
+        for excluded_path in excluded_paths:
             if path.startswith(excluded_path):
                 if excluded_path.endswith('/'):
                     return False
                 elif path == excluded_path[:-1]:
                     return False
-        return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Method to get the authorization header from a request.
