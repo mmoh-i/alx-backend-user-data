a5076d46c5411203ed042c72d8f51421af7468e2
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-11 10:48:57 +0100

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,19 +26,19 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized')
+@app_views.route('/unauthorized', methods=['GET'])
 def unauthorized():
     """ GET /api/v1/unauthorized
     Return:
      - the http response unauthorized
     """
-    abort(401)
+    abort(401, description="Unauthorized")
 
 
-@app_views.route('/forbidden')
+@app_views.route('/forbidden', methods=['GET'])
 def forbidden():
     """ GET /api/v1/forbidden
     Return:
      - the http returns forbidden
     """
-    abort(403)
+    abort(403, description="Forbidden")
