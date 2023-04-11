b86a7ca580b346b398860eebf060b67de98c10f8
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-11 17:19:42 +0100

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,6 +10,7 @@ authentication systems to be implemented. It provides three methods:
 """
 from typing import List, TypeVar
 from flask import request
+import re
 
 
 class Auth:
@@ -31,13 +32,9 @@ class Auth:
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
         for excluded_path in excluded_paths:
-            if path.startswith(excluded_path):
-                if excluded_path.endswith('/'):
-                    return False
-                elif path.endswith('/') == excluded_path[:-1]:
-                    return False
-                elif path == excluded_path:
-                    return False
+            pattern = re.escape(excluded_path).replace('\\*', '.*')
+            if re.match(pattern + '$', path):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
