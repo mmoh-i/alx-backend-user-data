45264c9c0b177e1cc5f1e06b59f3c7a8033b02bb
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-11 13:44:22 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,30 +11,27 @@ import base64
 class BasicAuth(Auth):
     """
     Class that implements Basic Authentication.
-
     Inherits from Auth.
     """
-    
-    
+
+
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """method to return Base64 athorization header.
         """
         if authorization_header is None or not isinstance(authorization_header, str):
-                return None
-
+            return None
         if not authorization_header.startswith('Basic '):
-                return None
-
+            return None
         return authorization_header.split(' ')[1]
 
 
     def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
         """method to decode vlue of base64 string"""
         if base64_authorization_header is None:
-          return None
+            return None
         if not isinstance(base64_authorization_header, str):
-          return None
+            return None
         try:
-          return base64.b64decode(base64_authorization_header).decode('utf-8')
+            return base64.b64decode(base64_authorization_header).decode('utf-8')
         except:
-          return None
+            return None
