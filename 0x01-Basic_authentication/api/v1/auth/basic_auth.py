f9bbf5bd87c4b76e14a0024659e8b9c2c657cf1f
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-11 13:51:49 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -25,7 +25,7 @@ class BasicAuth(Auth):
             return None
         return authorization_header.split(' ')[1]
 
-    def decode_base64_authorization_header(self,
+    def decode_base64_authorization_header(self,\
                                            base64_authorization_header: str) -> str:
         """method to decode vlue of base64 string"""
         if base64_authorization_header is None:
