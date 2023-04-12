4994087b73072795f96b439ab448b719dc22d984
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-12 16:17:37 +0100

    session_auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Session authentication
+
+"""
+from typing import TypeVar
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """Session Authentication"""
+    
+    pass
