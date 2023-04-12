86dd7dd97e0815ce00abea32b91f0d06a9c26260
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-12 16:01:19 +0100

    app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,8 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
-auth_type = getenv('AUTH_TYPE')
+auth_type = os.getenv("AUTH_TYPE")
 if auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -24,8 +23,10 @@ elif auth_type == "basic_auth":
 
 
 @app.before_request
-def before_request() -> None:
-    """Executes before the whole routes does"""
+def before_request():
+    """before Request to execute
+    before any route in the app.
+    """
     excluded_path = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
@@ -43,6 +44,29 @@ def before_request() -> None:
 
 @app.errorhandler(404)
 def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ request unauthorized
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ request forbidden
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
