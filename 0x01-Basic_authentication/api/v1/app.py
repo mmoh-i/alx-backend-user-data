ad9673d99ef278f3c19f5026111365b649a3846b
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-12 15:59:00 +0100

    app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,7 +13,8 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth_type = os.getenv("AUTH_TYPE")
+
+auth_type = getenv('AUTH_TYPE')
 if auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -23,10 +24,8 @@ elif auth_type == "basic_auth":
 
 
 @app.before_request
-def before_request():
-    """before Request to execute
-    before any route in the app.
-    """
+def before_request() -> None:
+    """Executes before the whole routes does"""
     excluded_path = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
@@ -43,6 +42,31 @@ def before_request():
 
 
 @app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized_req(error) -> str:
+    """Handles the 401 error and jsonify the error"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden_req(error) -> str:
+    """
+    Handles the 403 error and jsonify the error
+    403: User is authenticated but can't access a resource
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
 def not_found(error) -> str:
     """ Not found handler
     """
