634b5e28c6f2eabc97a9505aee0248208344b894
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-12 15:44:43 +0100

    Update users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -125,6 +125,7 @@ def update_user(user_id: str = None) -> str:
     user.save()
     return jsonify(user.to_json()), 200
 
+
 @app_views.route('/users/me', methods=['GET'], strict_slashes=False)
 def get_authenticated_user():
     """checks authenticated user available
