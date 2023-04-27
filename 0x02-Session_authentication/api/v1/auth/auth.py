212af544b1ebed114b136d3d8ddc0da6d8e810a6
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-27 13:47:08 +0100

    Update auth.py

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,48 +1,52 @@
 #!/usr/bin/env python3
-"""
-This module contains the implementation of the Auth class,
-which is used to manage API authentication.
-The Auth class is the template for all
-authentication systems to be implemented. It provides three methods:
-- require_auth: to require authentication for a path
-- authorization_header: to get the authorization header from a request
-- current_user: to get the current user from a request
-"""
-from typing import List, TypeVar
+"""authentication module"""
+
 from flask import request
-import re
+import fnmatch
+from typing import List, TypeVar
+import os
 
 
-class Auth:
-    """Class to manage API authentication.
-    This class is the template for all
-    authentication systems to be implemented.
+class Auth():
     """
+    Class to handle user authentication
+    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Method to require authentication for a path.
-        Args:
-            path (str): The path to require authentication for.
-            excluded_paths (List[str]): A list of paths that are excluded
-            from authentication.
-        Returns:
-            bool: Whether or not authentication is required.
-        """
+        """Public method for require auth"""
+
         if path is None:
             return True
-        if excluded_paths is None or len(excluded_paths) == 0:
+        elif excluded_paths is None or excluded_paths == []:
             return True
-        for excluded_path in excluded_paths:
-            pattern = re.escape(excluded_path).replace('\\*', '.*')
-            if re.match(pattern + '$', path):
-                return False
-        return True
+        else:
+            path = path.rstrip('/')
+            for excluded_path in excluded_paths:
+                excluded_path = excluded_path.rstrip('/')
+                if fnmatch.fnmatch(path, excluded_path):
+                    return False
+            return True
 
     def authorization_header(self, request=None) -> str:
-        """Method to get the authorization header from a request.
-        """
+        """A method to authorize the header requests"""
         if request is None:
             return None
         if not request.headers.get('Authorization'):
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user method"""
+        return None
+
+    def session_cookie(self, request=None):
+        """
+        Method that returns a cookie value from a request
+        """
+        if request is None:
+            return None
+        user_cookies = os.getenv('SESSION_NAME')
+        return request.cookies.get(user_cookies)
             return None
         return request.headers.get('Authorization')
 
