1cb8bc6fe3a4291b941edcee95c3964d27ea99be
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-27 13:51:12 +0100

    Create user_session.py

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for user session model"""
+
+from models.base import Base
+
+
+class UserSession(Base):
+    """User session class"""
+
+    def __init__(self, *args: list, **kwargs: dict) -> None:
+        """Initialize user session instances"""
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
